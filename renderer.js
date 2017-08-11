aca3adde2ca64f1f94b5e74e5acc2d1ba52a659c
Salvatore Josue Trimarchi Pinto <trimarchi@bacgroup.net>
2017-08-11 17:31:37 -0500

    [IMP] Login Form, OVD Comunication, Logos, XML Response Parse

--- renderer.js
+++ renderer.js
@@ -1,1 +1,48 @@
 // This is an empty file
+//    x = requests.post("", headers=custom_headers, data='')
+window.$ = window.jQuery = require('jquery');
+
+    $( "#connect" ).click(function() {
+
+    login = $( "#login" ).val()
+    pwd = $( "#pwd" ).val()
+    sm = $( "#sm" ).val()
+
+    //alert(login+pwd+sm);
+
+
+    $.ajax({
+      url:"https://"+sm+"/ovd/proxy.php",
+      type:"POST",
+      headers: {
+          "x-ovd-service": "start"
+      },
+      data: '<session mode="desktop" language="es" timezone="America/Guatemala"><user login="'+login+'" password="'+pwd+'"/></session>',
+      dataType: "text",
+      success:function(response){
+       //alert(response);
+
+
+        var xml = response,
+        xmlDoc = $.parseXML(xml),
+        $xml = $(xmlDoc);
+
+        $.each($xml.find('server'), function () {
+
+            login = $(this).attr("login");
+            fqdn = $(this).attr("fqdn");
+            port = $(this).attr("port");
+            password = $(this).attr("password");
+
+           alert(login+fqdn+port+password)
+
+        });
+
+
+
+      },
+      error:function(data){
+       alert("failure");
+      },
+    })
+});
