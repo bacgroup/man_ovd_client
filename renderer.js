e9bf29368f1ba88882a00d06cc0d971aa4aeffdb
Salvatore Josué Trimarchi Pinto <trimarchi@bacgroup.net>
2017-08-14 11:26:05 -0600

    [IMP] OSX and Linux Remote Desktop Feature :)

--- renderer.js
+++ renderer.js
@@ -1,16 +1,8 @@
-// This is an empty file
-//    x = requests.post("", headers=custom_headers, data='')
    window.$ = window.jQuery = require('jquery');
-
     $( "#connect" ).click(function() {
-
     login = $( "#login" ).val()
     pwd = $( "#pwd" ).val()
     sm = $( "#sm" ).val()
-
-    //alert(login+pwd+sm);
-
-
     $.ajax({
       url:"https://"+sm+"/ovd/proxy.php",
       type:"POST",
@@ -20,43 +12,46 @@
       data: '<session mode="desktop" language="es" timezone="America/Guatemala"><user login="'+login+'" password="'+pwd+'"/></session>',
       dataType: "text",
       success:function(response){
-       //alert(response);
-
-
         var xml = response,
         xmlDoc = $.parseXML(xml),
         $xml = $(xmlDoc);
 
         $.each($xml.find('server'), function () {
-
             login = $(this).attr("login");
             fqdn = $(this).attr("fqdn");
             port = $(this).attr("port");
             password = $(this).attr("password");
-
-           // alert(login+fqdn+port+password)
-
         });
 
-       var child = require('child_process').exec;
+     var executablePath;
+     if (process.platform == "linux") {
+      var child = require('child_process').exec;
+      executablePath = "/usr/bin/rdesktop -u "+login+" -p '"+password+"' "+fqdn+":"+port;
+      //alert(executablePath);
+      child(executablePath, function(error, stdout, stderr) {
+        //alert('stdout: ' + stdout);
+        //alert('stderr: ' + stderr);
+        if (error !== null) {
+            //alert('exec error: ' + error);
+        }
+    });
+    }
+    else if (process.platform == "darwin"){
+     var child = require('child_process').execFile;
+      executablePath = "/usr/bin/open";
+      opt = "rdp://"+login+":"+password+"@"+fqdn+":"+port+"?forwardDisks###yes\&forwardPrinters###yes";
+      options = [opt];
+    child(executablePath, options, function(error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+    });
 
-     if (process.platform = "Linux") {
-      //alert("Estas en Linux")
-      var executablePath = "/usr/bin/rdesktop -u "+login+" -p '"+password+"' "+fqdn+":"+port+" &";
-         //alert(executablePath)
-      }
-      else {
-      var executablePath = "/usr/bin/open";
-          var opt = "rdp://"+login+":"+password+"@"+fqdn+":"+port+"?forwardDisks###yes\&forwardPrinters###yes"
-      }
-      // RDESKTOP
-      //  var opt = "-u "+login+" -p "+password+" "+fqdn+":"+port
-       //alert(opt)
-       //var parameters = [opt];
+    }
 
-child(executablePath, function(err, data) {
-     //alert(data.toString())
-});
+
 
       },
       error:function(data){
