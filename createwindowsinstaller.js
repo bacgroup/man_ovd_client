1f43cf546b780adb23f4e4fb73b738e3150c287f
Salvatore Josué Trimarchi Pinto <trimarchi@bacgroup.net>
2017-09-25 14:59:15 -0600

    [ADD] Script create windows installer

---
+++ createwindowsinstaller.js
@@ -0,0 +1,7 @@
+var electronInstaller = require('electron-winstaller');
+resultPromise = electronInstaller.createWindowsInstaller({
+    appDirectory: '/tmp/build/my-app-64',
+    outputDirectory: '/tmp/build/installer64',
+    authors: 'My App Inc.',
+    exe: 'myapp.exe'
+  });
