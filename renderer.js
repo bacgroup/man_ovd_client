b595db2c4dd07e58bcd6f6040e390e4d0a756b1b
jet <jet@MacBook-Pro-Salvatore.local>
2017-09-25 11:59:04 -0600

    [FIX] MacFreeRDP

--- renderer.js
+++ renderer.js
@@ -36,6 +36,7 @@
                    var child = require('child_process').execSync;
                    //executablePath = __dirname+"/rdesktop/bin/rdesktop -u "+login+" -p '"+password+"' -f -r printer:`lpstat -d | cut -d':' -f2 | cut -d' ' -f2`=\"Ulteo TS Printer Driver\" -r disk:share=$HOME "+fqdn+":"+port;
                    executablePath = "open "+__dirname+"/MacFreeRDP/MacFreeRDP.app --args /f /v:" + fqdn + ":" + port + " /u:" + login + " /p:'" + password + "' /sec:rdp /drive:home,$HOME /printer +clipboard"
+                   //executablePath = "@rpath"+__dirname+"/xfreerdp_darwin/lib/ && "+__dirname+"/xfreerdp_darwin/bin/xfreerdp /f /v:" + fqdn + ":" + port + " /u:" + login + " /p:'" + password + "' /sec:rdp /drive:home,$HOME /printer +clipboard"
                    //alert(executablePath);
                    var child2 = require('child_process').execSync;
                        child(executablePath);
