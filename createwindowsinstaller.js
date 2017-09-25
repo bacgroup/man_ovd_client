293580e0b2d66c647e1610a02f9933e3dc04e00d
Salvatore Josué Trimarchi Pinto <trimarchi@bacgroup.net>
2017-09-25 15:02:58 -0600

    [IMP] Windows Installer

--- createwindowsinstaller.js
+++ createwindowsinstaller.js
@@ -1,7 +1,10 @@
 var electronInstaller = require('electron-winstaller');
+
 resultPromise = electronInstaller.createWindowsInstaller({
-    appDirectory: '/tmp/build/my-app-64',
-    outputDirectory: '/tmp/build/installer64',
-    authors: 'My App Inc.',
-    exe: 'myapp.exe'
+    appDirectory: 'packages/man_ovd_client-win32-ia32',
+    outputDirectory: 'packages/man_ovd_client-win32-ia32/man_ovd_client-win32-ia32_installer',
+    authors: 'MAN Consulting',
+    exe: 'man_ovd_client.exe'
   });
+
+resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
