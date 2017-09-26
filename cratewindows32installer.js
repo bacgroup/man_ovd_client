828de14347eebc2086bcc2fd0b9c19ee5b7e5b50
Salvatore Josué Trimarchi Pinto <trimarchi@bacgroup.net>
2017-09-26 13:20:33 -0600

    [ADD] WIN32

--- cratewindows32installer.js
+++ cratewindows32installer.js
@@ -10,14 +10,3 @@ resultPromise = electronInstaller.createWindowsInstaller({
   });
 
 resultPromise.then(() => console.log("Win32 Installer!"), (e) => console.log(`No dice: ${e.message}`));
-
-resultPromise1 = electronInstaller.createWindowsInstaller({
-    appDirectory: 'packages/man_ovd_client-win32-x64',
-    outputDirectory: 'packages/man_ovd_client-win32-x64_installer',
-    authors: 'MAN Consulting',
-    //exe: 'man_ovd_client.exe',
-    setupIcon: "icon.ico",
-    setupExe: 'Install.exe'
-  });
-
-resultPromise1.then(() => console.log("Win64 Installer!"), (e) => console.log(`No dice: ${e.message}`));
