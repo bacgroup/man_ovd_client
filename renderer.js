20674277ad8f282cd859de66a878c00de0967503
Salvatore Trimarchi <trimarchi@bacgroup.net>
2017-09-28 16:39:54 -0400

    [FIX] Code Quality

--- renderer.js
+++ renderer.js
@@ -1,5 +1,7 @@
-child_process = require('child_process')
+child = require('child_process').exec
 window.$ = window.jQuery = require('jquery');
+
+
 
 const notifier = require('node-notifier');
 
@@ -38,7 +40,8 @@ function start_session() {
                 res(response);
                 },
             error: function(data) {
-                rej({error : "Can't to Connect"});
+                $("#connect").animateCss('wobble');
+                rej("Unable to connect to OVD server");
             },
         });
 });
@@ -46,9 +49,9 @@ function start_session() {
 
 function validate_xml_response(xml) {
     return new Promise (function (res,rej) {
-        var xmlDoc = $.parseXML(xml),
+    try {
+        xmlDoc = $.parseXML(xml);
         $xml = $(xmlDoc);
-        try {
         $.each($xml.find('response'), function() {
             response = $(this).attr("code");
         });
@@ -60,54 +63,82 @@ function validate_xml_response(xml) {
         }
         catch(err)
         {
-            res("xml");
+            res(xml);
         }
     });
 }
-            
-            
-/*            
-               $.each($xml.find('server'), function() {
-                   login = $(this).attr("login");
-                   fqdn = $(this).attr("fqdn");
-                   port = $(this).attr("port");
-                   password = $(this).attr("password");
-               });
-               if (port == null) {
-                   port = 3389
-               };
-               var executablePath;
-               if (process.platform == "linux") {
-                   var child = require('child_process').exec;
-                   executablePath = "xfreerdp /v:" + fqdn + ":" + port + " /u:" + login + " /p:'" + password + "' /sec:rdp /drive:home,$HOME /printer +clipboard /jpeg /parent-window:`xwininfo -name 'MAN OVD Client' | grep 'Window id' | cut -d' ' -f4` /size:`xwininfo -name 'MAN OVD Client' | grep 'Width' | tr -s ' '|cut -d' ' -f3`x`xwininfo -name 'MAN OVD Client' | grep 'Height' | tr -s ' '|cut -d' ' -f3`";
-                   setTimeout(() => child(executablePath), 5000);
-               } else if (process.platform == "darwin") {
-                   var child = require('child_process').execSync;
-                   //executablePath = __dirname+"/rdesktop/bin/rdesktop -u "+login+" -p '"+password+"' -f -r printer:`lpstat -d | cut -d':' -f2 | cut -d' ' -f2`=\"Ulteo TS Printer Driver\" -r disk:share=$HOME "+fqdn+":"+port;
-                   executablePath = "open "+__dirname+"/MacFreeRDP/MacFreeRDP.app --args /f /v:" + fqdn + ":" + port + " /u:" + login + " /p:'" + password + "' /sec:rdp /drive:home,$HOME /printer +clipboard"
-                   //executablePath = "@rpath"+__dirname+"/xfreerdp_darwin/lib/ && "+__dirname+"/xfreerdp_darwin/bin/xfreerdp /f /v:" + fqdn + ":" + port + " /u:" + login + " /p:'" + password + "' /sec:rdp /drive:home,$HOME /printer +clipboard"
-                   //alert(executablePath);
-                   var child2 = require('child_process').execSync;
-                       child(executablePath);
-               }
-               //else if (process.platform == "win32"){
-               else {
-                   var child = require('child_process').exec;
-                   executablePath = "resources\\app\\rdp.exe /v:" + fqdn + ":" + port + " /u:" + login + " /p:" + password + " /printers /drives /max";
-                   setTimeout(() => child(executablePath), 5000);               }
 
-           },
-           error: function(data) {
-               alert("failure");
-           },
-       }) */
+function get_ovd_credentials(xml) {
+    return new Promise (function (res,rej) {
+    try {
+        xmlDoc = $.parseXML(xml);
+        $xml = $(xmlDoc);
+        params = {};
+         $.each($xml.find('server'), function() {
+           params['login'] = $(this).attr("login");
+           params['fqdn'] = $(this).attr("fqdn");
+           try{
+           params['port'] = $(this).attr("port");
+           }
+           catch(err)
+           {
+               params[port] = 3389
+           }
+           params['password'] = $(this).attr("password");
+         });
+        res(params);
+    }
+    catch(err)
+    {
+        $("#connect").animateCss('wobble');
+        rej("Unable to Login, OVD Server provides wrong Credential Data, "+err); 
+    }
+});
     
+}
 
-   
+function create_os_command(params) {
+    return new Promise (function (res,rej) {
+    try {    
+        os_rdp_exe = {
+            linux: "xfreerdp /v:" + params['fqdn'] + ":" + params['port'] + " /u:" + params['login'] + " /p:'" + params['password'] + "' /sec:rdp /drive:home,$HOME /printer +clipboard /jpeg /parent-window:`xwininfo -name 'MAN OVD Client' | grep 'Window id' | cut -d' ' -f4` /size:`xwininfo -name 'MAN OVD Client' | grep 'Width' | tr -s ' '|cut -d' ' -f3`x`xwininfo -name 'MAN OVD Client' | grep 'Height' | tr -s ' '|cut -d' ' -f3`",
+            win32: "resources\\app\\rdp.exe /v:" + params['fqdn'] + ":" + params['port'] + " /u:" + params['login'] + " /p:" + params['password'] + " /printers /drives /max",
+            darwin: "open "+__dirname+"/MacFreeRDP/MacFreeRDP.app --args /f /v:" + params['fqdn'] + ":" + params['port'] + " /u:" + params['login'] + " /p:'" + params['password'] + "' /sec:rdp /drive:home,$HOME /printer +clipboard"
+        }
+        res(os_rdp_exe[process.platform]);
+    }
+    catch(err) {
+        rej(err);
+    }
+        
+    })    
+}
 
-   $("#connect").click(function() {
-         start_session()
-        .then(xml => validate_xml_response(xml))
-        //.then(response => alert(response))
-        .catch(rejection => notify(__dirname+"/warning.png","Please contact your OVD Session Manager", "Reason for Rejection: "+rejection));
-   });
+function run_rdp(command){
+    return new Promise (function (res,rej) {
+        try {
+            notify(__dirname+"/conecting.png","Conecting to your OVD Session Manager", "Please wait a for a while...");
+            setTimeout(child(command),5000);
+            res('Done');
+        }
+        catch(err)
+        {
+            rej(err);
+        }
+        
+    });
+    
+}
+
+
+
+
+$("#connect").click(function() {
+     start_session()
+    .then(xml => validate_xml_response(xml))
+    .then(xml => get_ovd_credentials(xml))
+    .then(params => create_os_command(params))
+    .then(command => run_rdp(command))
+    //.then(request => alert(request))
+    .catch(rejection => notify(__dirname+"/warning.png","Please contact your OVD Session Manager", "Reason for Rejection: "+rejection));
+});
