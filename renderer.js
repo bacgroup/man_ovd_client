3ee4ede09be57a11f7293e62dc6a907936afe8c4
Salvatore Trimarchi <salvatore.trimarchi@gmail.com>
2018-03-21 14:54:36 -0600

    [IMP] OVD JAVA

--- renderer.js
+++ renderer.js
@@ -1,4 +1,5 @@
 /*jshint esversion: 6 */
+const remote = require('electron').remote
 child = require('child_process').exec;
 moment = require('moment');
 
@@ -34,217 +35,18 @@ function show_login(){
 
 $(".logo").click(function() {
     //$(this).animateCss('wobble');
-    window.open("https://manconsulting.co.uk","MAN Consulting", "width=1280,height=600")
+    window.open("https://manconsulting.co.uk","MAN Consulting", "width=1280,height=700")
 });
 
-
-function wait_for_ready_state()
-{
-        sm = "http://"+$("#sm").val();
-
-        options = {
-            method: 'POST',
-            headers: {'x-ovd-service': 'session_status', 'Cookie': 'PHPSESSID='+$("#login").val()}
-        };
-
-        client = request.createClient(options);
-        client(sm+"/ovd/proxy.php",function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(response.body);
-                setTimeout(function() {
-                xmlDoc = $.parseXML(response.body);
-                $xml = $(xmlDoc);
-                try {
-                $.each($xml.find('session'), function() {
-                    status = $(this).attr("status");
-                });
-                }
-                catch(err)
-                {
-                    return "None";
-                }
-                console.log(status);
-                return(status);
-                }, 2000);
-            }
-            else {
-                return "None";
-            }
-        });
-}
-
-
-function set_status(status, init=false) {
-    if (init != false) {
-        $(".status").empty();
-    }
-    now = moment().format('YYYY-MM-DD hh:mm:ss');
-    height = $(".status")[0].scrollHeight;
-    $(".status").html("<div class=\"col-xs-12\">"+status+"</div>");
-    $(".status").scrollTop(height);
-
-}
-
-function check_ovd_status(ovd_data) {
-    	return new Promise (function (res,rej) {
-        login = $("#login").val();
-        pwd = $("#pwd").val();
-        sm = "http://"+$("#sm").val();
-
-        options = {
-            method: 'POST',
-            headers: {'x-ovd-service': 'session_status', 'Cookie': 'PHPSESSID='+$("#login").val()}
-        };
-
-        client = request.createClient(options);
-        client(sm+"/ovd/proxy.php",function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(response.body);
-                setTimeout(function() {
-                xmlDoc = $.parseXML(response.body);
-                $xml = $(xmlDoc);
-                try {
-                $.each($xml.find('session'), function() {
-                    status = $(this).attr("status");
-                });
-                set_status("Preparing session settings");
-                }
-                catch(err)
-                {
-                    status = "None";
-                }
-                if(status == "logged")
-                {
-                    console.log(status);
-                    $('#inner_box').show();
-                    $('#progress').hide();
-                }
-                else if(status == "init")
-                {
-                    console.log(status);
-                    check_ovd_status(ovd_data);
-                }
-                else if (status == "ready") {
-                    console.log(response.body);
-
-                    validate_xml_response(ovd_data)
-                    .then(xml => get_ovd_credentials(xml))
-                    .then(params => create_os_command(params))
-                    .then(command => run_rdp(command))
-                    .then(check_ovd_status(ovd_data))
-                   .catch(rejection => {
-			    notify(__dirname+"/warning.png","Please contact your OVD Session Manager", "Reason: "+rejection);
-			    show_login();
-		            });
-                }
-                else {
-                    console.log(status);
-                    rej("Failed to connect");
-                }
-                }, 2000);
-            }
-            else {
-                $("#connect").animateCss('wobble');
-		show_login();
-                rej("Unable to connect to OVD server");
-		
-            }
-        });
-});
-}
-
-function start_session() {
-    	return new Promise (function (res,rej) {
-        login = $("#login").val();
-        pwd = $("#pwd").val();
-        sm = "http://"+$("#sm").val();
-             set_status("Starting OVD Session");
-        options = {
-            method: 'POST',
-            headers: {'x-ovd-service': 'start', 'Cookie': 'PHPSESSID='+$("#login").val()},
-            body: '<session mode="desktop"><user login="' + login + '" password="' + pwd + '"/></session>'
-        };
-
-        client = request.createClient(options);
-        client(sm+"/ovd/proxy.php",function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            notify(__dirname+"/conecting.png","Preparing session", "Please wait...");
-            res(response.body);
-        }
-        else {
-                $("#connect").animateCss('wobble');
-		show_login();
-                rej("Unable to connect to OVD server");
-        }
-        });
-});
-}
-
-function validate_xml_response(xml) {
+function create_os_command() {
     return new Promise (function (res,rej) {
-     set_status("Validate reponse");
-    try {
-        xmlDoc = $.parseXML(xml);
-        $xml = $(xmlDoc);
-        $.each($xml.find('response'), function() {
-            response = $(this).attr("code");
-        });
-        if(response != undefined && response != 'ReferenceError: response is not defined')
-        {
-            $("#connect").animateCss('wobble');
-            rej(response);
-        }
-        }
-        catch(err)
-        {
-            res(xml);
-        }
-    });
-}
-
-function get_ovd_credentials(xml) {
-    return new Promise (function (res,rej) {
-    try {
-        xmlDoc = $.parseXML(xml);
-        $xml = $(xmlDoc);
-        params = {};
-         $.each($xml.find('server'), function() {
-           params.login = $(this).attr("login");
-           params.fqdn = $(this).attr("fqdn");
-           try
-           {
-               params.port = $(this).attr("port");
-           }
-           catch(err) {
-               console.log("No hay puerto");
-           }
-           params.password = $(this).attr("password");
-         });
-           if(params.port == null || params.port == undefined || params.port == "undefined")
-           {
-               params.port = 3389;
-           }
-        res(params);
-    }
-    catch(err)
-    {
-        $("#connect").animateCss('wobble');
-	show_login();
-        rej("Unable to Login, OVD Server provides wrong Credential Data, "+err);
-    }
-});
-
-}
-
-function create_os_command(params) {
-    return new Promise (function (res,rej) {
-    st=$('select[name=st]').val();
+    //st=$('select[name=st]').val();
     sm=$("#sm").val();
     try {
         os_rdp_exe = {
-            linux: "java -jar  "+__dirname+"/OVDNativeClient/OVDNativeClient.jar -s "+sm+" -u "+$("#login").val()+" -p "+$("#pwd").val()+" -m "+st+" --progress-bar hide --auto-start",
-            win32: "resources\\app\\rdp.exe /v:" + params.fqdn + ":" + params.port + " /u:" + params.login + " /p:" + params.password + " /printers /drives /max",
-            darwin: "java -jar  "+__dirname+"/OVDNativeClient/OVDNativeClient.jar -s "+sm+" -u "+$("#login").val()+" -p "+$("#pwd").val()+" -m "+st+" --progress-bar hide"
+            linux: "java -jar  "+__dirname+"/OVDNativeClient/OVDNativeClient.jar -s "+sm+" -u "+$("#login").val()+" -p "+$("#pwd").val(),
+            //win32: "resources\\app\\rdp.exe /v:" + params.fqdn + ":" + params.port + " /u:" + params.login + " /p:" + params.password + " /printers /drives /max",
+            darwin: "java -jar  "+__dirname+"/OVDNativeClient/OVDNativeClient.jar -s "+sm+" -u "+$("#login").val()+" -p "+$("#pwd").val()
         };
         //alert(os_rdp_exe[process.platform])
         res(os_rdp_exe[process.platform]);
@@ -258,9 +60,7 @@ function create_os_command(params) {
 
 function run_rdp(command){
     return new Promise (function (res,rej) {
-	if (ready == true) {
-            try {
-                set_status("Starting Session");
+           try {
                 child(command);
                 console.log(command);
                 console.log("Corriendo RDP");
@@ -270,25 +70,34 @@ function run_rdp(command){
             {
                 rej(err);
             }
-	}
-	ready = false;
     });
-
 }
 
+function close_window(done){
+    return new Promise (function (res,rej) {
+           try {
+               remote.getCurrentWindow().close()
+            }
+            catch(err)
+            {
+                rej(err);
+            }
+    });
+}
+
+
 $("#connect").click(function() {
-     ready = true;
-     set_status("Connecting to OVD", true);
-     document.cookie = "PHPSESSID="+$("#login").val();
+     //set_status("Connecting to OVD", true);
      notify(__dirname+"/conecting.png","Conecting to your OVD Session Manager", "Please wait...");
      $('#inner_box').hide();
      $('#progress').show();
-     start_session()
-     .then(ovd_data => check_ovd_status(ovd_data))
+     create_os_command()
+     .then(command => run_rdp(command))
+     .then(result => close_window(result))
     .catch(rejection => { 
 	     notify(__dirname+"/warning.png","Please contact your OVD Session Manager", "Reason: "+rejection);
 	     show_login();
-	      });
+     });
 });
 
 $("input").change(function() {
